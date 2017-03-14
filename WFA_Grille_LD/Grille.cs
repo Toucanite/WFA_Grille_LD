bbc531ca3cd0b4e2d123e7a293a70d8e998b0c2f
Toucanite <lucas.dnt@eduge.ch>
2017-03-14 08:54:40 +0100

    Classe grille (pas testé)

--- WFA_Grille_LD/Grille.cs
+++ WFA_Grille_LD/Grille.cs
@@ -1,13 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WFA_Grille_LD
 {
     class Grille
     {
+        Point position;
+        Size taille;
+        int tailleCaseX, tailleCaseY, nbrCaseX, nbrCaseY;
+        Point[,] tPointsCaseX, tPointsCaseY;
 
+        public Grille(Point position, Size taille, int nbrCaseX, int nbrCaseY)
+        {
+            this.position = position;
+            this.taille = taille;
+            this.tailleCaseX = taille.Width / nbrCaseX;
+            this.tailleCaseY = taille.Height / nbrCaseY;
+            this.nbrCaseX = nbrCaseX;
+            this.nbrCaseY = nbrCaseY;
+
+            for (int i = 0; i < nbrCaseX; i++)
+            {
+                this.tPointsCaseX[i, 0] = new Point(position.X + i * tailleCaseX, position.Y);
+                this.tPointsCaseX[i, 1] = new Point(position.X + i * tailleCaseX, position.Y + taille.Height);
+            }
+
+            for (int i = 0; i < nbrCaseY; i++)
+            {
+                this.tPointsCaseY[i, 0] = new Point(position.Y + i * tailleCaseY, position.X);
+                this.tPointsCaseY[i, 1] = new Point(position.Y + i * tailleCaseY, position.X + taille.Width);
+            }
+
+        }
+
+
+        public void Paint(object sender, PaintEventArgs e)
+        {
+            Rectangle table = new Rectangle(position, taille);
+            e.Graphics.DrawRectangle(Pens.Black, table);
+
+            for (int i = 0; i < nbrCaseX; i++)
+			{
+                e.Graphics.DrawLine(Pens.Black, tPointsCaseX[i, 0], tPointsCaseX[i, 1]);
+			}
+
+            for (int i = 0; i < nbrCaseY; i++)
+            {
+                e.Graphics.DrawLine(Pens.Black, tPointsCaseY[i, 0], tPointsCaseY[i, 1]);
+            }
+
+        }
     }
 }
